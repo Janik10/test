f179b325329c60409d68980116f9067d28f0a422
Janik10 <mammadlidzahangir@gmail.com>
2025-05-24 04:19:42 -0700

    sd

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .routes import auth, books, readers, borrow
 
+# Создание всех таблиц в базе данных
 Base.metadata.create_all(bind=engine)
 
+# Инициализация приложения FastAPI
 app = FastAPI(title="Library System")
 
+# Подключение маршрутов (endpoints)
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(books.router, prefix="/books", tags=["Books"])
 app.include_router(readers.router, prefix="/readers", tags=["Readers"])
